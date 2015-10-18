816cabdb21eace2a87a2b5a7280899447f87613d
Akhil Narang <akhilnarang.1999@gmail.com>
2015-10-18 17:33:12 +0200

    fix
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,9 +64,9 @@ AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
 WITH_LZMA_OTA:= true
 
 #RR customizations
-TARGET_TC_ROM := 4.9-sm
+TARGET_TC_ROM := 5.2
 
-TARGET_TC_KERNEL := 4.9-sm
+TARGET_TC_KERNEL := 5.2
 
 #RR
 RROPTI := true
